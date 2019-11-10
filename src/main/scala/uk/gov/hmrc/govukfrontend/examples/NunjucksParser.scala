9bbed33fb660745fc372db7d123213f5b34ab857
Dario Abdulrehman <167463+dabd@users.noreply.github.com>
2019-11-10 00:49:31 +0000

    Finish parser implementation

--- src/main/scala/uk/gov/hmrc/govukfrontend/examples/NunjucksParser.scala
+++ src/main/scala/uk/gov/hmrc/govukfrontend/examples/NunjucksParser.scala
@@ -16,9 +16,12 @@
 
 package uk.gov.hmrc.govukfrontend.examples
 
+import fastparse.NoWhitespace._
 import fastparse._
-import NoWhitespace._
+import play.api.libs.json.Reads
 import play.twirl.api.Html
+import uk.gov.hmrc.govukfrontend.examples.AsJson._
+import uk.gov.hmrc.govukfrontend.views.html.components._
 
 case class NunjucksTemplate(imports: List[Import], body: List[NunjucksTemplateBody])
 case class Import(from: String, macroName: String)
@@ -26,22 +29,97 @@ case class Import(from: String, macroName: String)
 sealed trait NunjucksTemplateBody
 
 case class MacroCall(macroName: String, args: Any) extends NunjucksTemplateBody
-case class HtmlContent(content: Html) extends NunjucksTemplateBody
+case class TemplateHtml(content: Html) extends NunjucksTemplateBody
 case class WhiteSpace(ws: String) extends NunjucksTemplateBody
-
 
 object NunjucksParser {
 
-//  def nunjucksParser[_: P] = P(imports ~ ws ~ templateBody)
+  def nunjucksParser[_: P] = P(imports ~ ws ~ templateBody ~ End).map {
+    case (imports, templateBody) => NunjucksTemplate(imports = imports.toList, body = templateBody.toList)
+  }
 
-  def ws[_: P]: P[Unit] = P( (" " | "\n").rep(1) )
+  def ws[_: P] = P(CharsWhileIn(" \r\n", 0))
 
   def imports[_: P]: P[Seq[Import]] = P((importParser ~ ws).rep)
 
   def doubleQuotedString[_: P]: P[String] = P("\"" ~ (!"\"" ~ AnyChar).rep.! ~ "\"")
 
-  def importParser[_: P]: P[Import] = P( "{%" ~ ws ~ "from" ~ ws ~ doubleQuotedString ~ ws ~ "import" ~ ws ~ (!" " ~ AnyChar).rep.! ~ ws ~ "%}").map {
-    case (importStatement, macroName) => Import(from = importStatement, macroName = macroName)
+  def importParser[_: P]: P[Import] =
+    P("{%" ~ ws ~ "from" ~ ws ~ doubleQuotedString ~ ws ~ "import" ~ ws ~ (!" " ~ AnyChar).rep.! ~ ws ~ "%}").map {
+      case (importStatement, macroName) =>
+        Import(from = importStatement, macroName = macroName)
+    }
+
+  def macroName[_: P] = P(("govuk" ~ (!"(" ~ AnyChar).rep).!)
+
+  def macroCall[_: P]: P[MacroCall] =
+    P(ws ~ "{{" ~ ws ~ macroName ~ "(" ~ ("{" ~ ws ~ (!"})" ~ AnyChar).rep ~ ws ~ "}").! ~ ")" ~ ws ~ "}}").map {
+      case (m @ "govukAccordion", args) =>
+        jsonToMacroCall[Accordion](m, args)
+      case (m @ "govukBackLink", args) =>
+        jsonToMacroCall[BackLink](m, args)
+      case (m @ "govukBreadcrumbs", args) =>
+        jsonToMacroCall[Breadcrumbs](m, args)
+      case (m @ "govukButton", args) =>
+        jsonToMacroCall[Button](m, args)
+      case (m @ "govukCharacterCount", args) =>
+        jsonToMacroCall[CharacterCount](m, args)
+      case (m @ "govukCheckboxes", args) =>
+        jsonToMacroCall[Checkboxes](m, args)
+      case (m @ "govukDateInput", args) =>
+        jsonToMacroCall[DateInput](m, args)
+      case (m @ "govukDetails", args) =>
+        jsonToMacroCall[Details](m, args)
+      case (m @ "govukErrorMessage", args) =>
+        jsonToMacroCall[ErrorMessage](m, args)
+      case (m @ "govukErrorSummary", args) =>
+        jsonToMacroCall[ErrorSummary](m, args)
+      case (m @ "govukFieldset", args) =>
+        jsonToMacroCall[Fieldset](m, args)
+      case (m @ "govukFileUpload", args) =>
+        jsonToMacroCall[FileUpload](m, args)
+      case (m @ "govukFooter", args) =>
+        jsonToMacroCall[Footer](m, args)
+      case (m @ "govukHeader", args) =>
+        jsonToMacroCall[Header](m, args)
+      case (m @ "govukHint", args) =>
+        jsonToMacroCall[Hint](m, args)
+      case (m @ "govukInput", args) =>
+        jsonToMacroCall[Input](m, args)
+      case (m @ "govukInsetText", args) =>
+        jsonToMacroCall[InsetText](m, args)
+      case (m @ "govukLabel", args) =>
+        jsonToMacroCall[Label](m, args)
+      case (m @ "govukPanel", args) =>
+        jsonToMacroCall[Panel](m, args)
+      case (m @ "govukPhaseBanner", args) =>
+        jsonToMacroCall[PhaseBanner](m, args)
+      case (m @ "govukRadios", args) =>
+        jsonToMacroCall[Radios](m, args)
+      case (m @ "govukSelect", args) =>
+        jsonToMacroCall[Select](m, args)
+      case (m @ "govukSkipLink", args) =>
+        jsonToMacroCall[SkipLink](m, args)
+      case (m @ "govukSummaryList", args) =>
+        jsonToMacroCall[SummaryList](m, args)
+      case (m @ "govukTable", args) =>
+        jsonToMacroCall[Table](m, args)
+      case (m @ "govukTabs", args) =>
+        jsonToMacroCall[Tabs](m, args)
+      case (m @ "govukTag", args) =>
+        jsonToMacroCall[Tag](m, args)
+      case (m @ "govukTextarea", args) =>
+        jsonToMacroCall[Textarea](m, args)
+      case (m @ "govukWarningText", args) =>
+        jsonToMacroCall[WarningText](m, args)
+    }
+
+  def jsonToMacroCall[T: Reads](macroName: String, args: String): MacroCall = {
+    val templateParams = asJson(args).as[T]
+    MacroCall(macroName = macroName, args = templateParams)
   }
+
+  def templateBody[_: P]: P[Seq[NunjucksTemplateBody]] =
+    P(macroCall.rep)
 
 }
--- src/test/scala/uk/gov/hmrc/govukfrontend/examples/NunjucksParserSpec.scala
+++ src/test/scala/uk/gov/hmrc/govukfrontend/examples/NunjucksParserSpec.scala
@@ -17,62 +17,13 @@
 package uk.gov.hmrc.govukfrontend.examples
 
 import fastparse.Parsed.Success
+import fastparse._
 import org.scalatest.{Matchers, WordSpec}
-import play.api.libs.json.{JsObject, JsValue}
-import play.twirl.api.Html
+import uk.gov.hmrc.govukfrontend.examples.NunjucksParser._
 import uk.gov.hmrc.govukfrontend.views.html.components._
-import NunjucksParser._
-import fastparse.Parsed
 import scala.collection.mutable.ArrayBuffer
 
 class NunjucksParserSpec extends WordSpec with Matchers {
-
-//  "A parser for a Nunjucks template" should {
-//    "parse successfully" in {
-//
-//      val nunjucks =
-//        """{% from "govuk/components/error-summary/macro.njk" import govukErrorSummary %}
-//          |
-//          |{{ govukErrorSummary({
-//          |  titleText: "There is a problem",
-//          |  errorList: [
-//          |    {
-//          |      text: "The date your passport was issued must be in the past",
-//          |      href: "#passport-issued-error"
-//          |    },
-//          |    {
-//          |      text: "Enter a postcode, like AA1 1AA",
-//          |      href: "#postcode-error"
-//          |    }
-//          |  ]
-//          |}) }}""".stripMargin
-//
-//      fastparse.parse(nunjucks, nunjucksParser(_)) shouldBe Success(
-//        NunjucksTemplate(
-//          imports = List(
-//        Import(from = "govuk/components/error-summary/macro.njk", macroName = "govukErrorSummary")
-//          ),
-//          body = List(MacroCall(
-//            macroName = "govukErrorSummary",
-//            args = ErrorSummary(
-//              title = Text("There is a problem"),
-//              errorList = Seq(
-//                ErrorLink(
-//                  content = Text("The date your passport was issued must be in the past"),
-//                  href = "#passport-issued-error"
-//                ),
-//                ErrorLink(
-//                  content = Text("Enter a postcode, like AA1 1AA"),
-//                  href = "#postcode-error"
-//                )
-//              )
-//            )
-//          ))
-//        )
-//      )
-//
-//    }
-//  }
 
   "parse whitespace" should {
     "parse single line whitespace" in {
@@ -93,7 +44,8 @@ class NunjucksParserSpec extends WordSpec with Matchers {
       val s = "\"abc\""
 
       fastparse.parse(s, doubleQuotedString(_)) shouldBe Success(
-        "abc", 5
+        "abc",
+        5
       )
     }
   }
@@ -104,7 +56,7 @@ class NunjucksParserSpec extends WordSpec with Matchers {
 
       val Parsed.Success(parsedValue, _) = fastparse.parse(s, importParser(_))
 
-       parsedValue shouldBe
+      parsedValue shouldBe
         Import("govuk/components/error-summary/macro.njk", "govukErrorSummary")
     }
   }
@@ -112,17 +64,428 @@ class NunjucksParserSpec extends WordSpec with Matchers {
   "imports parser" should {
     "parse" in {
       val s =
-        """
-          |{% from "govuk/components/error-summary/macro.njk" import govukErrorSummary %}
+        """{% from "govuk/components/error-summary/macro.njk" import govukErrorSummary %}
           |{% from "govuk/components/date-input/macro.njk" import govukDateInput %}
           |""".stripMargin
 
       fastparse.parse(s, imports(_)) shouldBe Success(
-        Seq(
+        ArrayBuffer(
           Import("govuk/components/error-summary/macro.njk", "govukErrorSummary"),
           Import("govuk/components/date-input/macro.njk", "govukDateInput")
-        ), 100
+        ),
+        152
       )
     }
   }
+
+  "macroName parser" should {
+    "parse" in {
+      val s = """govukErrorSummary({"""
+
+      fastparse.parse(s, macroName(_)) shouldBe Success(
+        "govukErrorSummary",
+        17
+      )
+    }
+  }
+
+  "macroCall parser" should {
+    "parse" in {
+      val s =
+        """{{ govukErrorSummary({
+          |  titleText: "There is a problem",
+          |  errorList: [
+          |    {
+          |      text: "The date your passport was issued must be in the past",
+          |      href: "#passport-issued-error"
+          |    },
+          |    {
+          |      text: "Enter a postcode, like AA1 1AA",
+          |      href: "#postcode-error"
+          |    }
+          |  ]
+          |}) }}""".stripMargin
+
+      fastparse.parse(s, macroCall(_)) shouldBe Success(
+        MacroCall(
+          macroName = "govukErrorSummary",
+          args = ErrorSummary(
+            title = Text("There is a problem"),
+            errorList = Seq(
+              ErrorLink(
+                content = Text("The date your passport was issued must be in the past"),
+                href    = Some("#passport-issued-error")
+              ),
+              ErrorLink(
+                content = Text("Enter a postcode, like AA1 1AA"),
+                href    = Some("#postcode-error")
+              )
+            )
+          )
+        ),
+        289
+      )
+    }
+  }
+
+  "nunjucksParser" should {
+    "parse error summary example" in {
+      val s =
+        """{% from "govuk/components/error-summary/macro.njk" import govukErrorSummary %}
+          |
+          |{{ govukErrorSummary({
+          |  titleText: "There is a problem",
+          |  errorList: [
+          |    {
+          |      text: "The date your passport was issued must be in the past",
+          |      href: "#passport-issued-error"
+          |    },
+          |    {
+          |      text: "Enter a postcode, like AA1 1AA",
+          |      href: "#postcode-error"
+          |    }
+          |  ]
+          |}) }}""".stripMargin
+
+      fastparse.parse(s, nunjucksParser(_)) shouldBe Success(
+        NunjucksTemplate(
+          imports = List(
+            Import(from = "govuk/components/error-summary/macro.njk", macroName = "govukErrorSummary")
+          ),
+          body = List(
+            MacroCall(
+              macroName = "govukErrorSummary",
+              args = ErrorSummary(
+                title = Text("There is a problem"),
+                errorList = Seq(
+                  ErrorLink(
+                    content = Text("The date your passport was issued must be in the past"),
+                    href    = Some("#passport-issued-error")
+                  ),
+                  ErrorLink(
+                    content = Text("Enter a postcode, like AA1 1AA"),
+                    href    = Some("#postcode-error")
+                  )
+                )
+              )
+            ))
+        ),
+        369
+      )
+    }
+
+    "parse linking checkboxes radios example" in {
+      val s =
+        """{% from "govuk/components/error-summary/macro.njk" import govukErrorSummary %}
+          |{% from "govuk/components/checkboxes/macro.njk" import govukCheckboxes %}
+          |
+          |{{ govukErrorSummary({
+          |  "titleText": "There is a problem",
+          |  "errorList": [
+          |    {
+          |      "text": "Select if you are British, Irish or a citizen of a different country",
+          |      "href": "#nationality"
+          |    }
+          |  ]
+          |}) }}
+          |
+          |{{ govukCheckboxes({
+          |  idPrefix: "nationality",
+          |  name: "nationality",
+          |  fieldset: {
+          |    legend: {
+          |      text: "What is your nationality?",
+          |      isPageHeading: true,
+          |      classes: "govuk-fieldset__legend--xl"
+          |    }
+          |  },
+          |  hint: {
+          |    text: "If you have dual nationality, select all options that are relevant to you."
+          |  },
+          |  errorMessage: {
+          |    text: "Select if you are British, Irish or a citizen of a different country"
+          |  },
+          |  items: [
+          |    {
+          |      value: "british",
+          |      text: "British",
+          |      hint: {
+          |        text: "including English, Scottish, Welsh and Northern Irish"
+          |      },
+          |      id: "nationality"
+          |    },
+          |    {
+          |      value: "irish",
+          |      text: "Irish"
+          |    },
+          |    {
+          |      value: "other",
+          |      text: "Citizen of another country"
+          |    }
+          |  ]
+          |}) }}""".stripMargin
+
+      fastparse.parse(s, nunjucksParser(_)) shouldBe Success(
+        NunjucksTemplate(
+          imports = List(
+            Import(from = "govuk/components/error-summary/macro.njk", macroName = "govukErrorSummary"),
+            Import(from = "govuk/components/checkboxes/macro.njk", macroName    = "govukCheckboxes")
+          ),
+          body = List(
+            MacroCall(
+              macroName = "govukErrorSummary",
+              args = ErrorSummary(
+                errorList = Seq(
+                  ErrorLink(
+                    href    = Some("#nationality"),
+                    content = Text("Select if you are British, Irish or a citizen of a different country")
+                  )
+                ),
+                title = Text("There is a problem")
+              )
+            ),
+            MacroCall(
+              macroName = "govukCheckboxes",
+              args = Checkboxes(
+                idPrefix = Some("nationality"),
+                name     = "nationality",
+                fieldset = Some(
+                  Fieldset(
+                    legend = Some(
+                      Legend(
+                        content       = Text("What is your nationality?"),
+                        isPageHeading = true,
+                        classes       = "govuk-fieldset__legend--xl"
+                      ))
+                  )),
+                hint = Some(
+                  Hint(
+                    content = Text("If you have dual nationality, select all options that are relevant to you.")
+                  )),
+                errorMessage = Some(
+                  ErrorMessage(
+                    content = Text("Select if you are British, Irish or a citizen of a different country")
+                  )),
+                items = Seq(
+                  CheckboxItem(
+                    value   = "british",
+                    content = Text("British"),
+                    hint    = Some(Hint(content = Text("including English, Scottish, Welsh and Northern Irish"))),
+                    id      = Some("nationality")
+                  ),
+                  CheckboxItem(
+                    value   = "irish",
+                    content = Text("Irish")
+                  ),
+                  CheckboxItem(
+                    value   = "other",
+                    content = Text("Citizen of another country")
+                  )
+                )
+              )
+            )
+          )
+        ),
+        1124
+      )
+    }
+
+    "parse footer full example" in {
+      val s =
+        """{% from "govuk/components/footer/macro.njk" import govukFooter %}
+          |
+          |{{ govukFooter({
+          |  navigation: [
+          |    {
+          |      title: "Services and information",
+          |      columns: 2,
+          |      items: [
+          |        {
+          |          href: "#",
+          |          text: "Benefits"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Births, deaths, marriages and care"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Business and self-employed"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Childcare and parenting"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Citizenship and living in the UK"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Crime, justice and the law"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Disabled people"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Driving and transport"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Education and learning"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Employing people"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Environment and countryside"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Housing and local services"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Money and tax"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Passports, travel and living abroad"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Visas and immigration"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Working, jobs and pensions"
+          |        }
+          |      ]
+          |    },
+          |    {
+          |      title: "Departments and policy",
+          |      items: [
+          |        {
+          |          href: "#",
+          |          text: "How government works"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Departments"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Worldwide"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Policies"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Publications"
+          |        },
+          |        {
+          |          href: "#",
+          |          text: "Announcements"
+          |        }
+          |      ]
+          |    }
+          |  ],
+          |  meta: {
+          |    items: [
+          |      {
+          |        href: "#",
+          |        text: "Help"
+          |      },
+          |      {
+          |        href: "#",
+          |        text: "Cookies"
+          |      },
+          |      {
+          |        href: "#",
+          |        text: "Contact"
+          |      },
+          |      {
+          |        href: "#",
+          |        text: "Terms and conditions"
+          |      },
+          |      {
+          |        href: "#",
+          |        text: "Rhestr o Wasanaethau Cymraeg"
+          |      }
+          |    ],
+          |    html: 'Built by the <a href="#" class="govuk-footer__link">Government Digital Service</a>'
+          |  }
+          |}) }}""".stripMargin
+
+      fastparse.parse(s, nunjucksParser(_)) shouldBe Success(
+        NunjucksTemplate(
+          imports = List(
+            Import(from = "govuk/components/footer/macro.njk", macroName = "govukFooter")
+          ),
+          body = List(
+            MacroCall(
+              macroName = "govukFooter",
+              args = Footer(
+                navigation = Some(Seq(
+                  FooterNavigation(
+                    title   = Some("Services and information"),
+                    columns = Some(2),
+                    items = Some(Seq(
+                      FooterItem(href = Some("#"), text = Some("Benefits")),
+                      FooterItem(href = Some("#"), text = Some("Births, deaths, marriages and care")),
+                      FooterItem(href = Some("#"), text = Some("Business and self-employed")),
+                      FooterItem(href = Some("#"), text = Some("Childcare and parenting")),
+                      FooterItem(href = Some("#"), text = Some("Citizenship and living in the UK")),
+                      FooterItem(href = Some("#"), text = Some("Crime, justice and the law")),
+                      FooterItem(href = Some("#"), text = Some("Disabled people")),
+                      FooterItem(href = Some("#"), text = Some("Driving and transport")),
+                      FooterItem(href = Some("#"), text = Some("Education and learning")),
+                      FooterItem(href = Some("#"), text = Some("Employing people")),
+                      FooterItem(href = Some("#"), text = Some("Environment and countryside")),
+                      FooterItem(href = Some("#"), text = Some("Housing and local services")),
+                      FooterItem(href = Some("#"), text = Some("Money and tax")),
+                      FooterItem(href = Some("#"), text = Some("Passports, travel and living abroad")),
+                      FooterItem(href = Some("#"), text = Some("Visas and immigration")),
+                      FooterItem(href = Some("#"), text = Some("Working, jobs and pensions"))
+                    ))
+                  ),
+                  FooterNavigation(
+                    title = Some("Departments and policy"),
+                    items = Some(Seq(
+                      FooterItem(href = Some("#"), text = Some("How government works")),
+                      FooterItem(href = Some("#"), text = Some("Departments")),
+                      FooterItem(href = Some("#"), text = Some("Worldwide")),
+                      FooterItem(href = Some("#"), text = Some("Policies")),
+                      FooterItem(href = Some("#"), text = Some("Publications")),
+                      FooterItem(href = Some("#"), text = Some("Announcements"))
+                    ))
+                  )
+                )),
+                meta = Some(Meta(
+                  items = Some(Seq(
+                    FooterItem(href = Some("#"), text = Some("Help")),
+                    FooterItem(href = Some("#"), text = Some("Cookies")),
+                    FooterItem(href = Some("#"), text = Some("Contact")),
+                    FooterItem(href = Some("#"), text = Some("Terms and conditions")),
+                    FooterItem(href = Some("#"), text = Some("Rhestr o Wasanaethau Cymraeg"))
+                  )),
+                  content = HtmlContent(
+                    """Built by the <a href="#" class="govuk-footer__link">Government Digital Service</a>""")
+                ))
+              )
+            )
+          )
+        ),
+        2522
+      )
+
+    }
+  }
+
 }
