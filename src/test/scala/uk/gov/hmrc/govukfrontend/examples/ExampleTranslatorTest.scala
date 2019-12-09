52329f6192ae77d1c42ff0f465b5ab091d223fb6
omair-ali <omair.ali1@digital.hmrc.gov.uk>
2019-12-09 20:33:21 +0000

    PLATUI-301: WIP - Twirl example files auto translated from Nunjucks files

--- src/test/scala/uk/gov/hmrc/govukfrontend/examples/ExampleTranslatorTest.scala
+++ src/test/scala/uk/gov/hmrc/govukfrontend/examples/ExampleTranslatorTest.scala
@@ -15,16 +15,6 @@
  */
 
 package uk.gov.hmrc.govukfrontend.examples
+import org.scalatest._
 
-object PlayVersions {
-
-  sealed trait PlayVersion {
-    val version: Int
-    override def toString: String = version.toString
-  }
-
-  case class Play25() extends PlayVersion {val version: Int = 25}
-  case class Play26() extends PlayVersion {val version: Int = 26}
-
-  val implementedPlayVersions: Iterable[PlayVersion] = Iterable(Play25(), Play26())
-}
+class ExampleTranslatorTest extends WordSpec with Matchers {}
