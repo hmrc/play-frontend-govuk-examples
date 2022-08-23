9b9f2c36a6018c4aef74aa762c9d5bda328d69b3
Kyle Bowden <1393788+kyle-bowden@users.noreply.github.com>
2022-08-23 11:25:07 +0100

    PLATUI-1929: update govuk-frontend v4.3.1

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -6,7 +6,7 @@ import sbt.{ModuleID, Test}
 
 object LibDependencies {
 
-  val playFrontendHmrcVersion = "3.22.0"
+  val playFrontendHmrcVersion = "3.24.0"
 
   lazy val libDependencies: Seq[ModuleID] = dependencies(
     shared = {
