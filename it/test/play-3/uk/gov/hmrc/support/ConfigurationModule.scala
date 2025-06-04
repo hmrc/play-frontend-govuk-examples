37da1e451207ddf9ba3ba5a4e2d0d49365bed6f8
Joanna Pinto Paul <joanna.pinto.paul@digital.hmrc.gov.uk>
2025-06-04 15:12:38 +0100

    PLATUI-3748: Updating examples to latest govuk-frontend and hmrc-fron… (#77)
    PLATUI-3748: Updating examples to latest govuk-frontend and hmrc-frontend

--- it/test/play-3/uk/gov/hmrc/support/ConfigurationModule.scala
+++ it/test/play-3/uk/gov/hmrc/support/ConfigurationModule.scala
@@ -18,9 +18,11 @@ package uk.gov.hmrc.support
 
 import com.google.inject.{Binder, Module}
 import play.api.Configuration
-import uk.gov.hmrc.hmrcfrontend.config.TudorCrownConfig
+import uk.gov.hmrc.hmrcfrontend.config.{TudorCrownConfig, RebrandConfig}
 
-case class TudorCrownModule() extends Module {
-  override def configure(binder: Binder): Unit =
+case class ConfigurationModule() extends Module {
+  override def configure(binder: Binder): Unit = {
     binder.bind(classOf[TudorCrownConfig]).toInstance(TudorCrownConfig(Configuration.empty))
+    binder.bind(classOf[RebrandConfig]).toInstance(RebrandConfig(Configuration.empty))
+  }
 }
